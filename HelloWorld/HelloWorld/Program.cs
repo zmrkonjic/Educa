d7c5a839a5b7eb2ea876861892e6c4b6e9fd63e7
zmrkonjic <44403062+zmrkonjic@users.noreply.github.com>
2018-11-02 15:01:34 +0100

    Update Program.cs

--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -12,6 +12,7 @@ namespace HelloWorld
         {
             Console.WriteLine("Helo World");
             Console.WriteLine("Dodao promjenu u VS");
+            Console.WriteLine("Dodao promjenu u GitHub");
             Console.ReadLine();
 
         }

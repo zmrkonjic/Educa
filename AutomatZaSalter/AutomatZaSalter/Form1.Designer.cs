f4e1a38a6d5c0273b852f261767f44bebcd445e8
Zoran Mrkonjic <zoran.mrkonjic69@gmail.com>
2018-11-05 16:14:20 +0100

    Kreirao GUI

--- AutomatZaSalter/AutomatZaSalter/Form1.Designer.cs
+++ AutomatZaSalter/AutomatZaSalter/Form1.Designer.cs
@@ -28,13 +28,192 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.textBoxSalter1 = new System.Windows.Forms.TextBox();
+            this.textBoxSalter2 = new System.Windows.Forms.TextBox();
+            this.textBoxSalter3 = new System.Windows.Forms.TextBox();
+            this.btnNextSalter1 = new System.Windows.Forms.Button();
+            this.btnNextSalter2 = new System.Windows.Forms.Button();
+            this.btnNextSalter3 = new System.Windows.Forms.Button();
+            this.textBoxDisplej = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.btnPrintListic = new System.Windows.Forms.Button();
+            this.lblSalter1 = new System.Windows.Forms.Label();
+            this.lblSalter2 = new System.Windows.Forms.Label();
+            this.lblSalter3 = new System.Windows.Forms.Label();
+            this.lblInfo = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBoxSalter1
+            // 
+            this.textBoxSalter1.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.textBoxSalter1.Font = new System.Drawing.Font("Arial", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.textBoxSalter1.ForeColor = System.Drawing.SystemColors.Window;
+            this.textBoxSalter1.Location = new System.Drawing.Point(36, 69);
+            this.textBoxSalter1.Name = "textBoxSalter1";
+            this.textBoxSalter1.Size = new System.Drawing.Size(200, 29);
+            this.textBoxSalter1.TabIndex = 0;
+            // 
+            // textBoxSalter2
+            // 
+            this.textBoxSalter2.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.textBoxSalter2.Font = new System.Drawing.Font("Arial", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.textBoxSalter2.Location = new System.Drawing.Point(275, 69);
+            this.textBoxSalter2.Name = "textBoxSalter2";
+            this.textBoxSalter2.Size = new System.Drawing.Size(200, 29);
+            this.textBoxSalter2.TabIndex = 2;
+            // 
+            // textBoxSalter3
+            // 
+            this.textBoxSalter3.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.textBoxSalter3.Font = new System.Drawing.Font("Arial", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.textBoxSalter3.Location = new System.Drawing.Point(512, 69);
+            this.textBoxSalter3.Name = "textBoxSalter3";
+            this.textBoxSalter3.Size = new System.Drawing.Size(192, 29);
+            this.textBoxSalter3.TabIndex = 3;
+            // 
+            // btnNextSalter1
+            // 
+            this.btnNextSalter1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btnNextSalter1.Location = new System.Drawing.Point(36, 118);
+            this.btnNextSalter1.Name = "btnNextSalter1";
+            this.btnNextSalter1.Size = new System.Drawing.Size(192, 41);
+            this.btnNextSalter1.TabIndex = 4;
+            this.btnNextSalter1.Text = "Sljedeći";
+            this.btnNextSalter1.UseVisualStyleBackColor = true;
+            // 
+            // btnNextSalter2
+            // 
+            this.btnNextSalter2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btnNextSalter2.Location = new System.Drawing.Point(275, 118);
+            this.btnNextSalter2.Name = "btnNextSalter2";
+            this.btnNextSalter2.Size = new System.Drawing.Size(192, 41);
+            this.btnNextSalter2.TabIndex = 5;
+            this.btnNextSalter2.Text = "Sljedeći";
+            this.btnNextSalter2.UseVisualStyleBackColor = true;
+            // 
+            // btnNextSalter3
+            // 
+            this.btnNextSalter3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btnNextSalter3.Location = new System.Drawing.Point(512, 118);
+            this.btnNextSalter3.Name = "btnNextSalter3";
+            this.btnNextSalter3.Size = new System.Drawing.Size(192, 41);
+            this.btnNextSalter3.TabIndex = 6;
+            this.btnNextSalter3.Text = "Sljedeći";
+            this.btnNextSalter3.UseVisualStyleBackColor = true;
+            // 
+            // textBoxDisplej
+            // 
+            this.textBoxDisplej.BackColor = System.Drawing.Color.SeaGreen;
+            this.textBoxDisplej.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.textBoxDisplej.ForeColor = System.Drawing.Color.Yellow;
+            this.textBoxDisplej.Location = new System.Drawing.Point(36, 191);
+            this.textBoxDisplej.Name = "textBoxDisplej";
+            this.textBoxDisplej.Size = new System.Drawing.Size(668, 28);
+            this.textBoxDisplej.TabIndex = 7;
+            // 
+            // listBox1
+            // 
+            this.listBox1.Font = new System.Drawing.Font("Arial Black", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 26;
+            this.listBox1.Location = new System.Drawing.Point(319, 232);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(123, 108);
+            this.listBox1.TabIndex = 8;
+            // 
+            // btnPrintListic
+            // 
+            this.btnPrintListic.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btnPrintListic.Location = new System.Drawing.Point(311, 363);
+            this.btnPrintListic.Name = "btnPrintListic";
+            this.btnPrintListic.Size = new System.Drawing.Size(131, 36);
+            this.btnPrintListic.TabIndex = 9;
+            this.btnPrintListic.Text = "Printaj listić";
+            this.btnPrintListic.UseVisualStyleBackColor = true;
+            // 
+            // lblSalter1
+            // 
+            this.lblSalter1.AutoSize = true;
+            this.lblSalter1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.lblSalter1.ForeColor = System.Drawing.Color.Purple;
+            this.lblSalter1.Location = new System.Drawing.Point(79, 32);
+            this.lblSalter1.Name = "lblSalter1";
+            this.lblSalter1.Size = new System.Drawing.Size(103, 29);
+            this.lblSalter1.TabIndex = 10;
+            this.lblSalter1.Text = "Šalter 1";
+            // 
+            // lblSalter2
+            // 
+            this.lblSalter2.AutoSize = true;
+            this.lblSalter2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.lblSalter2.ForeColor = System.Drawing.Color.Purple;
+            this.lblSalter2.Location = new System.Drawing.Point(324, 32);
+            this.lblSalter2.Name = "lblSalter2";
+            this.lblSalter2.Size = new System.Drawing.Size(103, 29);
+            this.lblSalter2.TabIndex = 11;
+            this.lblSalter2.Text = "Šalter 2";
+            // 
+            // lblSalter3
+            // 
+            this.lblSalter3.AutoSize = true;
+            this.lblSalter3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.lblSalter3.ForeColor = System.Drawing.Color.Purple;
+            this.lblSalter3.Location = new System.Drawing.Point(555, 32);
+            this.lblSalter3.Name = "lblSalter3";
+            this.lblSalter3.Size = new System.Drawing.Size(103, 29);
+            this.lblSalter3.TabIndex = 12;
+            this.lblSalter3.Text = "Šalter 3";
+            // 
+            // lblInfo
+            // 
+            this.lblInfo.AutoSize = true;
+            this.lblInfo.Font = new System.Drawing.Font("Arial Black", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.lblInfo.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.lblInfo.Location = new System.Drawing.Point(81, 424);
+            this.lblInfo.Name = "lblInfo";
+            this.lblInfo.Size = new System.Drawing.Size(0, 24);
+            this.lblInfo.TabIndex = 13;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(745, 461);
+            this.Controls.Add(this.lblInfo);
+            this.Controls.Add(this.lblSalter3);
+            this.Controls.Add(this.lblSalter2);
+            this.Controls.Add(this.lblSalter1);
+            this.Controls.Add(this.btnPrintListic);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.textBoxDisplej);
+            this.Controls.Add(this.btnNextSalter3);
+            this.Controls.Add(this.btnNextSalter2);
+            this.Controls.Add(this.btnNextSalter1);
+            this.Controls.Add(this.textBoxSalter3);
+            this.Controls.Add(this.textBoxSalter2);
+            this.Controls.Add(this.textBoxSalter1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox textBoxSalter1;
+        private System.Windows.Forms.TextBox textBoxSalter2;
+        private System.Windows.Forms.TextBox textBoxSalter3;
+        private System.Windows.Forms.Button btnNextSalter1;
+        private System.Windows.Forms.Button btnNextSalter2;
+        private System.Windows.Forms.Button btnNextSalter3;
+        private System.Windows.Forms.TextBox textBoxDisplej;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button btnPrintListic;
+        private System.Windows.Forms.Label lblSalter1;
+        private System.Windows.Forms.Label lblSalter2;
+        private System.Windows.Forms.Label lblSalter3;
+        private System.Windows.Forms.Label lblInfo;
     }
 }
 

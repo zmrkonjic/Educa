fec3ef68f15e5a010b5ed34b7752b2a4e8a1ae4e
Zoran Mrkonjic <zoran.mrkonjic69@gmail.com>
2018-12-13 15:39:30 +0100

    prvi push aplikacije bookmark

---
+++ Bookmark/Bookmark.Manager/Bookmark.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+
+namespace Bookmark.Manager
+{
+    public class Bookmark
+    {
+        public Bookmark(string name, string url, DateTime timestamp)
+        {
+            this.Name = name;
+            this.Url = url;
+            this.Timestimp = timestamp;
+        }
+
+
+        public string Name { get; private set; }
+        public string Url { get; private set; }
+        public DateTime Timestimp { get; private set; }
+
+    }
+}
